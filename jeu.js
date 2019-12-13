03b432c319e28a2655f963d219c1eead66004e48
Nico-Daco <nicolas.dacosta@kolibree.com>
2019-12-13 19:30:17 +0100

    mise a jour

--- jeu.js
+++ jeu.js
@@ -7,6 +7,18 @@ var fenetreDeJeu = window.document.getElementById('fenetre');
 var fenetreStatut = window.document.getElementById('statut');
 var fenetreRegles = window.document.getElementById('regles');
 var restart = window.document.getElementById('restart');
+var container = window.document.getElementById('container');
+var imageDetective = window.document.getElementById('img1');
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+};
+
+function getRandomIntInterval(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 fenetreDeJeu.style.display = 'none';
 fenetreStatut.style.display = 'none';
@@ -30,36 +42,104 @@ restart.addEventListener("click",function(){
 
 // Fonction Constructeur pour génerer les fantomes
  var FabriqueDeFantome = function(){
-    this.hauteur ='121px';
-    this.largeur = '90px';
-    this.src = 'fantome-face.png';
-    this.position = 'absolute';
-    this.x = 0; //propriété pour le mouvement
-    this.y = 0; //propriété pour le mouvement
+     
+    this.fantome = new Image();
+    this.fantome.style.height = "121px";
+    this.fantome.style.width = "90px";
+    this.fantome.style.opacity ="0.7";
+    this.fantome.style.position = "absolute";
+    this.fantome.style.left = getRandomInt(800)+"px";
+    this.fantome.style.top = getRandomInt(500)+"px";
+    this.fantome.src = 'fantome-face.png';
 
-    this.initialisationFantome = function(){
-        var imageFantome = new Image(); //possibilité de mettre ca dans la fonction constructeur
-        imageFantome.src = this.src;
-        imageFantome.style.height = this.hauteur; 
-        imageFantome.style.width = this.largeur;
-        imageFantome.style.position = this.position;
-        imageFantome.style.left=this.x+"px";
-        imageFantome.style.top=this.y+"px";
-        fenetreDeJeu.appendChild(imageFantome);
+    this.directions = [-1, 1][getRandomInt(2)]
+    this.direction = getRandomIntInterval(1, 5);
+    this.limit = 1100;
+    this.limit2 = 650;
+
+    this.animation = function(){
+    if (parseInt(this.fantome.style.left) <= this.limit){
+
+        this.fantome.style.left = (parseInt(this.fantome.style.left)+ this.direction)+"px";
+        
+    }else{
+        this.limit = -100;
+        if(parseInt(this.fantome.style.left)>=this.limit){
+        this.fantome.style.left = (parseInt(this.fantome.style.left)+ this.direction*-1)+"px";
+        }
+        if(parseInt(this.fantome.style.left)<=0){
+        this.limit=1100;
+        }
     }
- }   
+    if (parseInt(this.fantome.style.top) <= this.limit2){
 
- var fantome = new FabriqueDeFantome();
- fantome.initialisationFantome();
- console.log(fantome)
+        this.fantome.style.top = (parseInt(this.fantome.style.top)+ this.direction)+"px";
+        
+    }else{
+        this.limit2 = -100;
+        if(parseInt(this.fantome.style.top)<=0){
+        this.limit2=650;
+        }
+        if(parseInt(this.fantome.style.top)>=this.limit2){
+        this.fantome.style.top = (parseInt(this.fantome.style.top)+ this.direction*-1)+"px";
+        }
+        
+    }
+    
+    
+    
+    requestAnimationFrame(this.animation);
+    }.bind(this)
 
-console.log(fantome.imageFantome)
+    fenetreDeJeu.appendChild(this.fantome);
+    requestAnimationFrame(this.animation);
+    return this;
+}
+
+//  var fantome = new FabriqueDeFantome;
+//  console.log("-------------------------")
+//  console.log(fantome)
+//  console.log("-------------------------")
+
+//  fantome.animation();
+ 
+
+//  Apparition de fantomes
+ var creationFantome= function(){ 
+    let i = 1
+    let interval = setInterval(function(){
+    if(i<=10){
+            new FabriqueDeFantome()
+            i++ 
+        } else {
+            clearInterval(interval)
+        }
+    }, 5000);
+}
+creationFantome();
+
+// Fonction constructeur Attrape Fantome
+var FabriqueDeBalle = function(){
+     
+    this.balle = new Image();
+    this.balle.style.height = "20px";
+    this.balle.style.width = "20px";
+    this.balle.style.position = "absolute";
+    this.balle.style.left =(parseInt(container.style.left) + parseInt(container.style.width) /2) +"px";
+    this.balle.style.top = (parseInt(container.style.top) + parseInt(container.style.height) /2) +"px";
+    this.balle.src = 'attrapeFantome.png';
+    this.balle.style.display="block";
+    
+    fenetreDeJeu.appendChild(this.balle);
+    return this.balle;
+ }  
+
 
 
 //Mouvement du personnage
 
 window.addEventListener("load", function(){
-    var container = window.document.getElementById('container');
+    
     container.style.position="absolute";
     container.style.height="160px";
     container.style.width="73px";
@@ -67,28 +147,26 @@ window.addEventListener("load", function(){
     container.style.left= "560px";
     container.style.top="450px";  
     
-    var imageDetective = window.document.getElementById('img1');
+   
     imageDetective.style.position="absolute";
     imageDetective.style.width="333px";
     
 
     var imagePositionX = parseInt(container.style.left);
     var imagePositionY= parseInt(container.style.top);
-    this.console.log(imagePositionY);
     var imagedivx=0;
     var imagedivy=0;
 
-    // imageObelix.style.left= 0;
-    // imageObelix.style.top=0;
+
     
     // Projectile contre les fantomes
 
-    var attrapeballe = window.document.getElementById('balle');
-    attrapeballe.style.position = "absolute";
-    attrapeballe.style.height = "20px";
-    attrapeballe.style.left = (parseInt(container.style.left) + parseInt(container.style.width) /2) +"px";
-    attrapeballe.style.top = (parseInt(container.style.top) + parseInt(container.style.height) /2) +"px";
-    attrapeballe.style.display = 'none';
+    // var attrapeballe = window.document.getElementById('balle');
+    // attrapeballe.style.position = "absolute";
+    // attrapeballe.style.height = "20px";
+    // attrapeballe.style.left = (parseInt(container.style.left) + parseInt(container.style.width) /2) +"px";
+    // attrapeballe.style.top = (parseInt(container.style.top) + parseInt(container.style.height) /2) +"px";
+    // attrapeballe.style.display = 'none';
 
 
 
@@ -105,6 +183,7 @@ window.addEventListener("load", function(){
             // if(parseInt(imagePositionX) <= 0){
             //     imagePositionX = 10;
             // }
+            // arrow left
             imagePositionX -= 10;
             container.style.left = imagePositionX + "px";
             // console.log(imagePositionX);
@@ -166,6 +245,11 @@ window.addEventListener("load", function(){
             }
             break;
 
+            case 32: 
+            var balle = new FabriqueDeBalle();
+
+
+            break;
             
         }
     }
